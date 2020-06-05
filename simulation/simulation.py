7026e6d84c41b4c918f9ff4fab250a29858c4134
Andrey Scherbovich <andrey.scherbovich@gmail.com>
2020-06-05 16:15:09 +0200

    split plots

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -7,7 +7,8 @@ import numpy as np
 idp = 1
 spos_token_supply = 1
 
-curve_coefficient = 0.5 # configurable dynamic param
+curve_coefficient = 0.5  # configurable dynamic param
+
 
 def process_incoming_DAI_fee(fee):
     global idp, spos_token_supply, curve_coefficient
@@ -18,12 +19,14 @@ def process_incoming_DAI_fee(fee):
     idp += fee
     spos_token_supply += to_mint_tokens
 
+
 def test():
     global idp, spos_token_supply
-    n = 30000 # total payments
+    n = 30000  # total payments
     np.random.seed(1234)
-    payments = np.random.rand(n) * 20 # random payments up to 20 DAI
-    token_prices = [idp / spos_token_supply] # token prices in DAI after payments
+    payments = np.random.rand(n) * 20  # random payments up to 20 DAI
+    # token prices in DAI after payments
+    token_prices = [idp / spos_token_supply]
     pools = [idp]
     tokens = [spos_token_supply]
     for i in range(n):
@@ -32,10 +35,11 @@ def test():
         pools.append(idp)
         tokens.append(spos_token_supply)
 
-    fig, (ax1, ax2) = plt.subplots(2, 1)
+    fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
     ax1.plot(token_prices, label="token price in DAI")
     ax1.legend(loc='upper left')
     ax2.plot(pools, label="incentivization DAI pool")
-    ax2.plot(tokens, label="tokens")
     ax2.legend(loc='upper left')
+    ax3.plot(tokens, label="tokens")
+    ax3.legend(loc='upper left')
     plt.show()
